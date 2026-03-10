2428371920803684f7b90adb1a8211f696b67680
thomas-ding <42778175+tom-d-77@users.noreply.github.com>
2026-03-10 08:49:22 +0100

    Update NodeDataLoader.php
    NodeDataLoader: Double quotes turned to single quotes, IF turned to CASE WHEN, 0 to false. Changes done to work with PostgreSQL.

--- Neos.ContentRepository.LegacyNodeMigration/Classes/Helpers/NodeDataLoader.php
+++ Neos.ContentRepository.LegacyNodeMigration/Classes/Helpers/NodeDataLoader.php
@@ -23,15 +23,15 @@ final class NodeDataLoader implements \IteratorAggregate
             FROM
                 neos_contentrepository_domain_model_nodedata
             WHERE
-                workspace = "live"
-                AND (movedto IS NULL OR removed=0)
-                AND path != "/"
+                workspace = \'live\'
+                AND (movedto IS NULL OR removed=false)
+                AND path != \'/\'
             ORDER BY
                 -- dimensionshash d7... is {} (the empty dimension).
                 -- Because there is a fallback from a dimension value to no dimension value in the old CR (if nothing is found),
                 -- we need to ensure that the empty dimensionshash comes LAST.
                 -- see NodeDataToEventsProcessor::processNodeData() which handles this special case
-                parentpath, sortingindex, path, IF(dimensionshash=\'d751713988987e9331980363e24189ce\', 1, 0)
+                parentpath, sortingindex, path, CASE WHEN dimensionshash=\'d751713988987e9331980363e24189ce\' THEN 1 ELSE 0 END
         ');
         return $query->iterateAssociative();
     }
